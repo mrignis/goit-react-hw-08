034c1c27a6bfa1610f46988f22ed4ad07b06388d
mrignis <chelbarah9678@gmail.com>
2024-04-23 00:02:42 -0400

    up

--- src/components/Form/SectionHome.jsx
+++ src/components/Form/SectionHome.jsx
@@ -5,10 +5,7 @@ const SectionHome = () => {
   return (
     <section className="section-home">
       <h2>Welcome to our website!</h2>
-      <p>
-        This is the home section where you can introduce your website or
-        application.
-      </p>
+      <p></p>
       <div>
         <Link to="/register">
           <button>Register</button>
