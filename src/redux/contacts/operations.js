1e09c6a567c09c091256416bed288679c1941424
mrignis <chelbarah9678@gmail.com>
2024-04-25 11:28:11 -0400

    up

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// код операцій fetchContacts, addContact, deleteContact
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,71 +1,56 @@
-// slice.js
-
 import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "@reduxjs/toolkit";
+import { fetchContacts, addContact, deleteContact } from "./operations";
 
 const initialState = {
   items: [],
-  filter: "",
-  loading: false,
   error: null,
 };
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState,
-  reducers: {
-    fetchContactsRequest(state) {
-      state.loading = true;
-      state.error = null;
-    },
-    fetchContactsSuccess(state, { payload }) {
-      state.items = payload.sort((a, b) => a.name.localeCompare(b.name));
-      state.loading = false;
-    },
-    fetchContactsError(state, { payload }) {
-      state.loading = false;
-      state.error = payload;
-    },
-    addContactRequest(state) {
-      state.loading = true;
-      state.error = null;
-    },
-    addContactSuccess(state, { payload }) {
-      state.items.unshift(payload);
-      state.loading = false;
-    },
-    addContactError(state, { payload }) {
-      state.loading = false;
-      state.error = payload;
-    },
-    deleteContactRequest(state) {
-      state.loading = true;
-      state.error = null;
-    },
-    deleteContactSuccess(state, { payload }) {
-      state.items = state.items.filter(({ id }) => id !== payload);
-      state.loading = false;
-    },
-    deleteContactError(state, { payload }) {
-      state.loading = false;
-      state.error = payload;
-    },
-    changeFilter(state, { payload }) {
-      state.filter = payload;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(addContact.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.items = [...state.items, action.payload];
+        state.error = null;
+      })
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        // Видалення контакту за його id
+        state.items = state.items.filter((item) => item.id !== action.payload);
+        state.error = null;
+      });
   },
 });
 
-export const {
-  fetchContactsRequest,
-  fetchContactsSuccess,
-  fetchContactsError,
-  addContactRequest,
-  addContactSuccess,
-  addContactError,
-  deleteContactRequest,
-  deleteContactSuccess,
-  deleteContactError,
-  changeFilter,
-} = contactsSlice.actions;
+export const selectContacts = (state) => state.contacts.items;
+export const selectNameFilter = (state) => state.filters.nameFilter;
 
-export default contactsSlice.reducer; // Змінив експорт
+// Переписуємо селектор, щоб фільтрувати контакти за іменем
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, nameFilter) => {
+    // Фільтруємо контакти за іменем, включаючи тільки ті, які відповідають фільтру
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(nameFilter.toLowerCase())
+    );
+  }
+);
+
+const { reducer: contactsReducer } = contactsSlice;
+export default contactsReducer;
